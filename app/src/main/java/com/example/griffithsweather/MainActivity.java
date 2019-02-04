8d7b0e61ee3c1f09eec69e0acd46909ce3e10a0a
Pawel Rudnicki <p.j.rudnicki@gmail.com>
2019-02-04 00:08:18 +0000

    Added splash screen

--- app/src/main/java/com/example/griffithsweather/MainActivity.java
+++ app/src/main/java/com/example/griffithsweather/MainActivity.java
@@ -8,6 +8,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // go back to default theme after splash screen
+        setTheme(R.style.AppTheme);
         setContentView(R.layout.activity_main);
     }
 }
