491219bf0f37360c0a590c20daee9630df802309
Pawel Rudnicki <p.j.rudnicki@gmail.com>
2019-02-08 16:22:14 +0000

    rollback

--- app/src/main/java/com/example/griffithsweather/MainActivity.java
+++ app/src/main/java/com/example/griffithsweather/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private boolean trolo;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
